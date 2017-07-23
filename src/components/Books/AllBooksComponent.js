9340c347dbfd7f0fc9d53489ce7529ac5d93c29c
Noah Guillory <slokomisu4@gmail.com>
2017-07-23 18:29:06 -0500

    Add masonry to project

--- src/components/Books/AllBooksComponent.js
+++ src/components/Books/AllBooksComponent.js
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Header } from 'semantic-ui-react'
+import { Grid, Header, Image } from 'semantic-ui-react'
+import Masonry from 'react-masonry-component'
+import { connect } from 'react-redux';
 
-function AllBooksComponent (props) {
-  return (
-    <Grid.Row>
-      <Grid.Column>
-        <Header textAlign="center" size="huge">
-          Welcome to the Book Trade Club!
-          <Header.Subheader>Check out the collections of your fellow bookworms and trade with them.</Header.Subheader>
-        </Header>
-      </Grid.Column>
+class AllBooksComponent extends Component {
 
-    </Grid.Row>
-  )
+
+
+  render () {
+
+    const childElement = this.props.books.map(book => {
+      return (
+        <Image src={book.coverImage} />
+      )
+    })
+
+    return (
+      <Grid.Row>
+        <Grid.Column>
+          <Header textAlign="center" size="huge">
+            Welcome to the Book Trade Club!
+            <Header.Subheader>Check out the collections of your fellow bookworms
+              and trade with them.</Header.Subheader>
+          </Header>
+        </Grid.Column>
+        <Grid.Row>
+          <Masonry options={{columnWidth: 200}}>
+            {/*<Image.Group>*/}
+              {childElement}
+            {/*</Image.Group>*/}
+          </Masonry>
+        </Grid.Row>
+
+      </Grid.Row>
+    )
+  }
 }
 
 AllBooksComponent.propTypes = {}
 AllBooksComponent.defaultProps = {}
 
-export default AllBooksComponent
+const mapState = ({books: {bookList}}) => {
+  return {
+    books: bookList
+  }
+}
+
+export default connect(mapState)(AllBooksComponent);
